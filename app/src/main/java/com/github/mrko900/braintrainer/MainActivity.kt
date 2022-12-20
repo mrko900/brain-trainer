665143ef514d249bddaabf9b954f7c4579852ae0
mrko900 <mrko900@hotmail.com>
2022-12-20 19:11:12 +0300

    Navigation animations

--- app/src/main/java/com/github/mrko900/braintrainer/MainActivity.kt
+++ app/src/main/java/com/github/mrko900/braintrainer/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.github.mrko900.braintrainer
 
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavAction
+import androidx.core.view.iterator
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import com.github.mrko900.braintrainer.databinding.MainBinding
 
@@ -23,6 +24,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainBinding
     private lateinit var navigation: NavController
 
+    fun getMenuIndices(menu: Menu): Map<Int, Int> {
+        val res = HashMap<Int, Int>()
+        var i = 0
+        for (item in menu) {
+            res[item.itemId] = i++
+        }
+        return res
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,15 +40,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         navigation = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
-        val testnav1 = View.generateViewId()
-        val testnav2 = View.generateViewId()
-        navigation.findDestination(R.id.fragment_home)!!.putAction(testnav1,
-            NavAction(R.id.fragment_options))
-        navigation.findDestination(R.id.fragment_options)!!.putAction(testnav2,
-            NavAction(R.id.fragment_home))
+
+        val animRight: NavOptions = NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_right_fade_in)
+            .setExitAnim(R.anim.slide_out_right_fade_out)
+            .build()
+        val animLeft: NavOptions = NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_left_fade_in)
+            .setExitAnim(R.anim.slide_out_left_fade_out)
+            .build()
+
+        val menuIndices = getMenuIndices(binding.navView.menu)
 
         binding.navView.setOnItemSelectedListener {
-            navigation.navigate(if (it.itemId == R.id.home) testnav2 else testnav1)
+            if (binding.navView.selectedItemId == it.itemId)
+                return@setOnItemSelectedListener false
+            val slideLeft = menuIndices.getValue(it.itemId) > menuIndices.getValue(binding.navView.selectedItemId)
+            navigation.navigate(getFragmentId(it.itemId),
+                navOptions = if (slideLeft) animLeft else animRight, args = null)
             return@setOnItemSelectedListener true
         }
     }
