715eb27d1fff95f4ff662b4067643b569312e574
mrko900 <mrko900@hotmail.com>
2022-12-21 19:55:00 +0300

    Recycler view spacing

--- app/src/main/java/com/github/mrko900/braintrainer/ExerciseListFragment.kt
+++ app/src/main/java/com/github/mrko900/braintrainer/ExerciseListFragment.kt
@@ -1,13 +1,15 @@
 package com.github.mrko900.braintrainer
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.mrko900.braintrainer.databinding.ExerciseListBinding
+import kotlin.math.roundToInt
 
 class ExerciseListFragment : Fragment() {
     private lateinit var binding: ExerciseListBinding
@@ -23,7 +25,17 @@ class ExerciseListFragment : Fragment() {
     }
 
     private fun initListView(listView: RecyclerView) {
-        listView.layoutManager = LinearLayoutManager(context)
+        val spanCount = resources.getInteger(R.integer.exercise_list_span_count)
+        listView.layoutManager = GridLayoutManager(context, spanCount)
         listView.adapter = ExerciseListViewAdapter(layoutInflater)
+        listView.addItemDecoration(
+            ExerciseListViewItemDecoration(
+                TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    resources.getInteger(R.integer.exercise_list_spacing_dp).toFloat(),
+                    resources.displayMetrics
+                ).roundToInt(), spanCount
+            )
+        )
     }
 }
--- app/src/main/java/com/github/mrko900/braintrainer/ExerciseListView.kt
+++ app/src/main/java/com/github/mrko900/braintrainer/ExerciseListView.kt
@@ -1,6 +1,8 @@
 package com.github.mrko900.braintrainer
 
+import android.graphics.Rect
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.github.mrko900.braintrainer.databinding.ExerciseListItemBinding
@@ -9,7 +11,9 @@ class ExerciseListViewAdapter(private val layoutInflater: LayoutInflater) :
     RecyclerView.Adapter<ExerciseListViewAdapterViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseListViewAdapterViewHolder {
-        return ExerciseListViewAdapterViewHolder(layoutInflater, parent)
+        val viewHolder = ExerciseListViewAdapterViewHolder(layoutInflater, parent)
+        viewHolder.itemView.layoutParams.height = 400
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ExerciseListViewAdapterViewHolder, position: Int) {
@@ -29,5 +33,16 @@ class ExerciseListViewAdapterViewHolder : RecyclerView.ViewHolder {
     private constructor(binding: ExerciseListItemBinding) :
             super(binding.root) {
         this.binding = binding
+    }
+}
+
+class ExerciseListViewItemDecoration(private val space: Int, private val columns: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        val nItems: Int = ((parent.adapter?.itemCount
+            ?: throw IllegalArgumentException("RecyclerView has no adapter")) + columns - 1) / columns * columns
+        outRect.left = if (parent.getChildLayoutPosition(view) % columns == 0) space * 2 else space
+        outRect.right = if ((parent.getChildLayoutPosition(view) - columns + 1) % columns == 0) space * 2 else space
+        outRect.top = if (parent.getChildLayoutPosition(view) < columns) space * 2 else space
+        outRect.bottom = if (parent.getChildLayoutPosition(view) >= nItems - columns) space * 2 else space
     }
 }
