ba494afe82bc6d91f4058d75601d919dc58c85b8
mrko900 <mrko900@hotmail.com>
2023-03-06 23:30:35 +0300

    Exercise surface view added

--- app/src/main/java/com/github/mrko900/braintrainer/ExerciseFragment.kt
+++ app/src/main/java/com/github/mrko900/braintrainer/ExerciseFragment.kt
@@ -1,8 +1,12 @@
 package com.github.mrko900.braintrainer
 
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PixelFormat
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -24,6 +28,25 @@ class ExerciseFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.surface.setZOrderOnTop(true)
+        binding.surface.holder.setFormat(PixelFormat.TRANSPARENT)
+        binding.surface.holder.addCallback(ExerciseSurfaceHolderCallback())
+    }
+}
 
+class ExerciseSurfaceHolderCallback : SurfaceHolder.Callback {
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        val canvas = holder.lockCanvas()
+        val paint = Paint()
+        paint.color = Color.RED
+        paint.style = Paint.Style.FILL
+        canvas.drawCircle(200f, 200f, 50f, paint)
+        holder.unlockCanvasAndPost(canvas)
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
     }
 }
