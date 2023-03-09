4c87e36e5435f55481c8796bf1c7a4ce44dd8216
mrko900 <mrko900@hotmail.com>
2023-03-09 18:30:18 +0300

    Commit

--- app/src/main/java/com/github/mrko900/braintrainer/ShapeFusionExercise.kt
+++ app/src/main/java/com/github/mrko900/braintrainer/ShapeFusionExercise.kt
@@ -1,21 +1,42 @@
 package com.github.mrko900.braintrainer
 
-import android.graphics.Color
-import android.graphics.Paint
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import androidx.core.util.Consumer
+
+data class Shape(private val matrix: List<List<Boolean>>, private val width: Int, private val height: Int) {
+    fun getHeight(): Int {
+        return height
+    }
+
+    fun getWidth(): Int {
+        return width
+    }
+
+    fun isSet(x: Int, y: Int): Boolean {
+        if (x < 0 || x >= width || y < 0 || y >= height)
+            throw IllegalArgumentException("coords not in range")
+        return matrix[x][y]
+    }
+}
+
+data class ShapeFusionExerciseQuestion(private val choices: List<Shape>, val answer: Int) {
+    init {
+        if (!(answer >= 0 && answer < choices.size))
+            throw IllegalArgumentException("answer out of bounds")
+    }
+}
 
 class ShapeFusionExercise(onFinishedCallback: Consumer<ExerciseResult>) : AbstractExercise(onFinishedCallback) {
     override fun init(surface: SurfaceView) {
         surface.holder.addCallback(object : SurfaceHolder.Callback {
             override fun surfaceCreated(holder: SurfaceHolder) {
-                val canvas = holder.lockCanvas()
-                val paint = Paint()
-                paint.color = Color.GREEN
-                paint.style = Paint.Style.FILL
-                canvas.drawCircle(300f, 200f, 170f, paint)
-                holder.unlockCanvasAndPost(canvas)
+//                val canvas = holder.lockCanvas()
+//                val paint = Paint()
+//                paint.color = Color.GREEN
+//                paint.style = Paint.Style.FILL
+//                canvas.drawCircle(300f, 200f, 170f, paint)
+//                holder.unlockCanvasAndPost(canvas)
             }
 
             override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
