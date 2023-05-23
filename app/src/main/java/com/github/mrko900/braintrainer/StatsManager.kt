3698e2fe4ed504390fe771e12cbafbddac3bc87f
mrko900 <mrko900@hotmail.com>
2023-05-23 14:57:52 +0300

    Commit

--- app/src/main/java/com/github/mrko900/braintrainer/StatsManager.kt
+++ app/src/main/java/com/github/mrko900/braintrainer/StatsManager.kt
@@ -1,11 +1,21 @@
 package com.github.mrko900.braintrainer
 
+import java.util.Date
+
 class StatsManager {
     // null = all exercises
     fun getGeneralStats(exercise: ExerciseMode?): GeneralStats {
         return GeneralStats(
             3, 4, 5, 6, 7, 8.910f, 11f, 8f, 11, 12.13f, -14.15f, 16.17f, 181.9202122f
         )
+    }
+
+    fun getRatingHistory(exercise: ExerciseMode): History {
+        return object : History {
+            override fun valueAt(date: Date): Int? {
+                return 25
+            }
+        }
     }
 }
 
@@ -24,3 +34,7 @@ data class GeneralStats(
     val avgImprovementPerHour: Float,
     val totalTimeSpent: Float
 )
+
+interface History {
+    fun valueAt(date: Date): Int?
+}
--- app/src/main/java/com/github/mrko900/braintrainer/StatsProgressFragment.kt
+++ app/src/main/java/com/github/mrko900/braintrainer/StatsProgressFragment.kt
@@ -7,7 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavOptions
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mrko900.braintrainer.databinding.StatsProgressBinding
+import java.util.Calendar
+import java.util.Date
+import java.util.GregorianCalendar
 
 class StatsProgressFragment : Fragment() {
     private lateinit var binding: StatsProgressBinding
@@ -40,5 +48,34 @@ class StatsProgressFragment : Fragment() {
             )
         }
         mainActivity.supportActionBar!!.title = mainActivity.getString(R.string.stats_progress)
+
+        val data = ArrayList<Entry>()
+        data.add(Entry((GregorianCalendar(2023, 3, 3).time.time / 86400000L).toFloat(), 1f))
+        data.add(Entry((GregorianCalendar(2023, 3, 4).time.time / 86400000L).toFloat(), 2f))
+        data.add(Entry((GregorianCalendar(2023, 3, 5).time.time / 86400000L).toFloat(), -1f))
+        data.add(Entry((GregorianCalendar(2023, 3, 6).time.time / 86400000L).toFloat(), 7f))
+        val chart = binding.chart
+        val dataSet = LineDataSet(data, "Test")
+        dataSet.circleRadius = 4f
+        dataSet.circleHoleRadius = 2.5f
+        dataSet.lineWidth = 2f
+        chart.legend.isEnabled = false
+        chart.description.isEnabled = false
+        chart.data = LineData(dataSet)
+        chart.xAxis.setDrawGridLines(false)
+        val xAxis = binding.chart.xAxis
+        xAxis.granularity = 1f
+        xAxis.valueFormatter = object : ValueFormatter() {
+            override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+                val date = Calendar.getInstance()
+                date.time = Date(86400000L * value.toLong())
+                val month = date.get(Calendar.MONTH)
+                val months = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+                val monthName = months[month]
+                val dayOfMonth = date.get(Calendar.DAY_OF_MONTH).toString()
+                return "$monthName $dayOfMonth"
+            }
+        }
+        chart.invalidate()
     }
 }
